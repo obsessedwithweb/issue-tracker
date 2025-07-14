3885aeff21904ac466231c5d2baab20575af99f5
RMA <rma@gmail.com>
2025-07-14 21:23:29 +0100

    Build Alert dialog for deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,9 +1,37 @@
-import { Button } from "@radix-ui/themes"
+import { AlertDialog, Button, Flex } from "@radix-ui/themes"
 
 
 const DeleteIssueButton = ({ issueID }: { issueID: number }) => {
   return (
-    <Button color="red">Delete issue</Button>
+    <AlertDialog.Root>
+
+      <AlertDialog.Trigger>
+        <Button color="red">Delete issue</Button>
+      </AlertDialog.Trigger>
+
+      <AlertDialog.Content > {/*maxWidth="450px"*/ }
+
+        <AlertDialog.Title>Revoke access</AlertDialog.Title>
+
+        <AlertDialog.Description size="2">
+          Are you sure? This issue will no longer be accessible and any
+          existing data will be deleted.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
